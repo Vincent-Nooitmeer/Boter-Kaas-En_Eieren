af71a813726e2803c9e8695cdb13e57ef7672019
Vincent Nooitmeer <172208@student.horizoncollege.nl>
2023-02-20 21:57:17 +0100

    files en beginsel

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Boter kaas en eieren</title>
+</head>
+
+<body>
+    <script src="script.js"></script>
+    <div class="row-1">
+        <button></button>
+        <button></button>
+        <button></button>
+    </div>
+
+    <div class="row-2">
+        <button></button>
+        <button></button>
+        <button></button>
+    </div>
+
+    <div class="row-3">
+        <button></button>
+        <button></button>
+        <button></button>
+    </div>
+</body>
+
+</html>
